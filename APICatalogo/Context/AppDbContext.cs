90d3af030399f2d1cddf53f669fbaa1cfc484901
Samuel-Lucas <samuel.moreira10@outlook.com>
2021-05-30 11:59:53 -0300

    Mapeando classes e aplicando migration

---
+++ APICatalogo/Context/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using APICatalogo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalogo.Context
+{
+    public class AppDbContext : DbContext   // Permite Coordenar as classes para o modelo de dados
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)   // Operações (crud)
+            : base(options)
+        {
+
+        }
+
+        // Mapeiam entidades
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
